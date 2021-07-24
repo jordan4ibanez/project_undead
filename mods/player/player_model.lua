a39fa7b4aa53702b0b0b556d61ee98f8d71e9a64
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-24 19:12:28 -0400

    Allow model to intercept player controls using get_player_control_bits

--- mods/player/player_model.lua
+++ mods/player/player_model.lua
@@ -1,5 +1,9 @@
 local get_player_by_name = minetest.get_player_by_name
 
+local player = nil
+local old_control_bits = 0
+local control_bits = 0
+
 minetest.register_entity(":player_model",{
     visual = "mesh",
     mesh = "player.b3d",
@@ -9,14 +13,76 @@ minetest.register_entity(":player_model",{
     attached_player = nil,
     timer = 0,
     on_step = function(self, dtime)
-        self.timer = self.timer + dtime
-        if (self.timer >= 0.5) then
-            self.timer = 0
-            if (self.attached_player == nil or get_player_by_name(self.attached_player) == nil) then
-                self.object:remove()
-                return
-            end
+        if (self.attached_player == nil) then
+            self.object:remove()
+            return
         end
+
+        player = get_player_by_name(self.attached_player)
+
+        if (player == nil) then
+            self.object:remove()
+            return
+        end
+
+        -- a cache happy way to intercept player controls
+
+        control_bits = player:get_player_control_bits()
+        old_control_bits = control_bits
+
+        -- zoom
+        if (control_bits >= 512) then
+            control_bits = control_bits - 512
+        end
+
+        -- place
+        if (control_bits >= 256) then
+            control_bits = control_bits - 256
+        end
+
+        -- dig
+        if (control_bits >= 128) then
+            control_bits = control_bits - 128
+        end
+
+        -- sneak
+        if (control_bits >= 64) then
+            control_bits = control_bits - 64
+        end
+
+        -- aux1
+        if (control_bits >= 32) then
+            control_bits = control_bits - 32
+        end
+
+        -- jump
+        if (control_bits >= 16) then
+            control_bits = control_bits - 16
+        end
+
+        -- right
+        if (control_bits >= 8) then
+            control_bits = control_bits - 8
+        end
+
+        -- left
+        if (control_bits >= 4) then
+            control_bits = control_bits - 4
+        end
+
+        -- down
+        if (control_bits >= 2) then
+            control_bits = control_bits - 2
+        end
+
+        -- up
+        if (control_bits >= 1) then
+            control_bits = control_bits - 1
+        end
+
+
+        -- do animation
+
     end
 })
 
