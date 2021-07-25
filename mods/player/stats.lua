7f5081e50eec39001c5ab2352290eb57ee686c26
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-24 20:17:05 -0400

    Add stats documentation

--- mods/player/stats.lua
+++ mods/player/stats.lua
@@ -2,6 +2,18 @@
 -- hierarchy = local table of players - player - status value
 local stats = {}
 
+--[[
+health - how healthy you are
+hunger - how hungry you are
+thirst - how thirsty you are
+exhaustion - can limit your speed/stamina
+panic - how panicked you are, can cause you to miss swings and not be able to sleep
+infection - once bit, this will start climbing
+sadness - can cause a bunch of issues
+strength - how much you can carry, how hard you can swing
+fitness - how fast you can run and for how long
+]]--
+
 minetest.register_on_joinplayer(function(player)
     stats[player:get_player_name()] = {
         health = 100,
