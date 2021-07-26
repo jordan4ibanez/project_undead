8c38e496bedb075831e526131e9315915938888e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-26 13:28:01 -0400

    add in all_texture to register_tile()

--- mods/terrain/tile_builder.lua
+++ mods/terrain/tile_builder.lua
@@ -6,8 +6,12 @@ function register_tile(def)
     if (def.rotation) then
         param2 = "facedir"
     end
-    register_node(":" .. (def.name or "you've failed to name your tile"), {
-        description = def.description,
+
+    local tiles
+
+    if (def.all_texture) then
+        tiles = {def.all_texture}
+    else
         tiles = {
             def.top_texture or "invisible.png",
             def.bottom_texture or "invisible.png",
@@ -15,7 +19,12 @@ function register_tile(def)
             def.back_texture or "invisible.png",
             def.right_texture or "invisible.png",
             def.left_texture or "invisible.png",
-        },
+        }
+    end
+
+    register_node(":" .. (def.name or "you've failed to name your tile"), {
+        description = def.description,
+        tiles = tiles,
         paramtype2 = param2,
         pointable = def.pointable or (build_mode or false),
         diggable = build_mode or false,
