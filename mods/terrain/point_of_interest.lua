ef45dd2ab64e78a10e0233d5842cad7941711021
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-26 21:42:55 -0400

    Add in documentation for point of interest (zombie spawning algorithm)

--- mods/terrain/point_of_interest.lua
+++ mods/terrain/point_of_interest.lua
@@ -1,2 +1,14 @@
+-- this is a table which defines where zombies/hordes spawn
 
--- this is a tile which goes under the map to spawn zombies/hordes
+--[[
+pos - base position of point of interest
+hordes - allow 30-50 zombies to spawn at once
+amount_normal = amount that will normally spawn during spawn hours (night time)
+chance - every time the server "ticks" to spawning, it will do a random number and compare this to it (0-100), the higher the chance, the higher the possibility zombies will spawn
+radius - how far away zombies can spawn, zombies will only spawn on road surfaces or inside buildings
+]]--
+
+local interest_table = {
+    --east highway gas station
+    {pos = { x = 700, z = 11}, hordes = true, amount_normal = 10, chance = 80, radius = 40}
+}
