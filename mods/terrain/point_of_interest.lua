dd126e4b2fb1f4461edafe8fa5cba3004b7ad714
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-26 22:12:38 -0400

    Update point_of_interest.lua

--- mods/terrain/point_of_interest.lua
+++ mods/terrain/point_of_interest.lua
@@ -1,7 +1,13 @@
 local get_timeofday = minetest.get_timeofday
+local random = math.random
 
+-- cached data
 local timer = 0
 local current_time = 0
+
+local function roll_dice()
+    return(random() * 100)
+end
 
 
 -- this is a table which defines where zombies/hordes spawn
@@ -9,12 +15,14 @@ local current_time = 0
 --[[
 pos - base position of point of interest
 hordes - allow 30-50 zombies to spawn at once
+horde_chance - how often zombies will normally spawn in a horde (0-100%), higher is higher chance
 amount_normal = amount that will normally spawn during spawn hours (night time)
-chance - every time the server "ticks" to spawning, it will do a random number and compare this to it (0-100), the higher the chance, the higher the possibility zombies will spawn
+chance - how often zombies will normally spawn (0-100%), higher is higher chance
 radius - how far away zombies can spawn, zombies will only spawn on road surfaces or inside buildings
 
 time_begin - the earliest zombies can start spawning, must be less than midnight, as in 7:00pm (19:00) for example
 time_end - the latest time zombies stop spawning, must be after midnight, as in 5:00am (05:00) for example
+spawn_frequency - how often, in seconds, the map will try to spawn zombies/hordes at points of interest
 
 a note:
 time comparison is quite easy and simple
@@ -29,22 +37,40 @@ time comparison is quite easy and simple
 
 local time_begin = 20000
 local time_end = 4800
+local spawn_frequency = 1
 
 
 local interest_table = {
-    --east highway gas station
-    {pos = { x = 700, z = 11}, hordes = true, amount_normal = 10, chance = 80, radius = 40}
+    -- east highway gas station
+    {pos = { x = 700, z = 11}, hordes = true, horde_chance = 50, amount_normal = 10, chance = 80, radius = 40}
 }
 
 
-
 minetest.register_globalstep(function(dtime)
-    --this must be multiplied to get a relatable time scale
 
-    current_time = get_timeofday() * 24000
+    timer = timer + dtime
+    -- only initialize every spawn_frequency seconds
+    if (timer >= spawn_frequency) then
+        -- this must be multiplied to get a relatable time scale
+        current_time = get_timeofday() * 24000
+        -- only spawn between acceptable times
+        if (current_time > time_begin or current_time < time_end) then
+            -- run through each point of interest
+            for _,point in pairs(interest_table) do
+                -- roll the virtual dice to see if this will spawn more zombies
+                if (roll_dice() < point.chance) then
+                    -- now roll the dice and see if a horde will spawn
+                    if (roll_dice() < point.horde_chance) then
+                        -- someone's about to have a bad day
+                        
+                    else
+                        -- spawn zombies in amount_normal amount
 
-    if (current_time > time_begin or current_time < time_end) then
-        print("it's spawning time!")
+
+                    end
+                end
+            end
+        end
     end
-
 end)
+
