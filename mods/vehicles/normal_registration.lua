62d994a6e3f5a6507bf641280da090510d482895
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-30 18:09:31 -0400

    Add Huxton Roadster

--- mods/vehicles/normal_registration.lua
+++ mods/vehicles/normal_registration.lua
@@ -1,9 +1,39 @@
 -- 4 door sedan
 register_vehicle({
-    name = "huxton_roadster"
+    name = "huxton_roadster",
+    info = "Huxton Roadster",
+    class = "muscle",
+
+    mesh = "huxton_roadster.obj",
+    texture = "huxton_roadster_bronze.png",
+    scale = 26,
+    wheel = "normal",
+    wheel_animation_multiplier = 5,
+
+    -- power, brake, friction = 0 - 1000
+    power = 250,
+    brake_force = 300,
+    air_resistance = 60, -- higher means car slows down faster
+    -- do not exceed 80 - outrun map loading
+    max_speed = 50,
+    max_reverse_speed = -6,
+
+    front_wheel_scale = { x = 5, y = 6},
+    front_track_width = 13,
+    front_suspension_height = 10.5,
+    front_wheel_base = 28,
+
+    rear_wheel_scale = { x = 5, y = 6},
+    rear_track_width = 13,
+    rear_suspension_height = 10.5,
+    rear_wheel_base = 18.5,
+
+    vehicle_height = 2.3,
+    height_offset = -1.65,
+    vehicle_width = 1.4,
 })
 
---- 4 door sports car
+-- 4 door sports car
 register_vehicle({
     name = "hearken_autobahn",
     info = "Hearken Autobahn 2.2L",
@@ -34,6 +64,6 @@ register_vehicle({
     rear_wheel_base = 13.6,
 
     vehicle_height = 1.95,
-    vehicle_width = 0.9,
     height_offset = -0.9,
+    vehicle_width = 0.9,
 })
