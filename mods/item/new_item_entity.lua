35ce5ac7b28756d26332981ec932ac008cf3c2f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-04 12:29:35 -0400

    Add prototype item entity

---
+++ mods/item/new_item_entity.lua
@@ -0,0 +1,37 @@
+local add_entity = minetest.add_entity
+
+minetest.register_entity(":item", {
+    initial_properties = {
+        hp_max           = 1,
+        visual           = "sprite",
+        physical         = false,
+        textures         = {""},
+        is_visible       = true,
+        pointable        = true,
+        collide_with_objects = false,
+        collisionbox = {0,0,0,0,0,0},
+        selectionbox = {-0.21, -0.21, -0.21, 0.21, 0.21, 0.21},
+        visual_size  = {x = 0.21, y = 0.21},
+    },
+
+    set_item = function(self, item)
+        local stack = ItemStack(item or self.itemstring)
+        self.itemstring = stack:to_string()
+        if self.itemstring == "" then
+            self.object:remove()
+            return
+        end
+        local itemname = stack:is_known() and stack:get_name() or "unknown"
+        self.object:set_properties({
+            textures = {itemname},
+            wield_item = self.itemstring,
+        })
+    end,
+})
+
+function add_item(pos, item_name)
+    local new_item = add_entity(pos, "item")
+    if (new_item) then
+        new_item:set_item(item_name)
+    end
+end
