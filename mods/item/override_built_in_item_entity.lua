51ea22788cc292ab252976db6f88356c17f48ff5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-04 12:25:24 -0400

    Free up memory/processing power with built in item entity

--- mods/item/override_built_in_item_entity.lua
+++ mods/item/override_built_in_item_entity.lua
@@ -1,40 +1,17 @@
+-- free up memory
 minetest.register_entity(":__builtin:item", {
-    initial_properties = {
-        hp_max           = 1,
-        visual           = "sprite",
-        physical         = false,
-        textures         = {""},
-        is_visible       = true,
-        pointable        = true,
-
-        collide_with_objects = false,
-        collisionbox = {0,0,0,0,0,0},
-        selectionbox = {-0.21, -0.21, -0.21, 0.21, 0.21, 0.21},
-        visual_size  = {x = 0.21, y = 0.21},
-    },
-    set_item = function(self, item)
-        local stack = ItemStack(item or self.itemstring)
-        self.itemstring = stack:to_string()
-        if self.itemstring == "" then
-            -- item not yet known
-            return
-        end
-        local itemname = stack:is_known() and stack:get_name() or "unknown"
-        self.object:set_properties({
-            textures = {itemname},
-            wield_item = self.itemstring,
-        })
-    end,
-    on_activate = function(self, staticdata, dtime_s)
-        if string.sub(staticdata, 1, string.len("return")) == "return" then
-            local data = minetest.deserialize(staticdata)
-            if data and type(data) == "table" then
-                self.itemstring = data.itemstring
-            end
-        else
-            self.itemstring = staticdata
-        end
-        self.object:set_armor_groups({immortal = 1})
-        self.set_item(self,self.itemstring)
-    end
+    initial_properties = nil,
+    set_item = nil,
+    on_activate = nil,
+    itemstring = nil,
+    moving_state = nil,
+    physical_state = nil,
+    age = nil,
+    get_staticdata = nil,
+    try_merge_with = nil,
+    enable_physics = nil,
+    disable_physics = nil,
+    on_step = nil,
+    on_punch = nil
 })
+
