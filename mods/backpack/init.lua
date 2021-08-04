33caf3bd1a9d710b392a3328517a92a87d64f737
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-04 04:41:26 -0400

    Add a closed backpack

---
+++ mods/backpack/init.lua
@@ -0,0 +1,23 @@
+register_tile({
+    name = "backpack",
+
+    top_texture = "backpack_top.png",
+    bottom_texture = "backpack_bottom.png^[transformFY",
+    front_texture = "backpack_side.png",
+    back_texture = "backpack_side.png^[transformFX",
+    left_texture = "backpack_front.png",
+    right_texture = "backpack_back.png",
+
+    pixel_box_texture_size = 32,
+    pixel_box = {
+        {
+          5,
+          5,
+          0,
+
+          27,
+          32,
+          13
+        }
+    }
+})
