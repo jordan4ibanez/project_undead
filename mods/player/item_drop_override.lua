40b07b7a44287520d912fdf0e17a90b23b62e212
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-08 18:25:04 -0400

    Free up Q key for usage

---
+++ mods/player/item_drop_override.lua
@@ -0,0 +1,37 @@
+local getn = table.getn
+
+-- modular item_drop overrides which frees up the Q key
+
+local function_tables = {}
+
+minetest.item_drop = function(itemstack, player, pos)
+    for _,func in pairs(function_tables) do
+        func(itemstack, player, pos)
+    end
+    return nil
+end
+
+function allocate_drop_button(func)
+    function_tables[getn(function_tables) + 1] = func
+end
+
+
+
+allocate_drop_button(
+function(itemstack,player,pos)
+    print("wow this works")
+end
+)
+
+
+allocate_drop_button(
+        function(itemstack,player,pos)
+            print("Why is the minetest api written like this?")
+        end
+)
+
+allocate_drop_button(
+        function(itemstack,player,pos)
+            print("this is a horrible workaround")
+        end
+)
