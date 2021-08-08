43fd247f335aa3f2c85edcf9d6dc2985b7342974
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-08 19:56:28 -0400

    Fix player inventory

--- mods/player/inventory_adjustments.lua
+++ mods/player/inventory_adjustments.lua
@@ -1,4 +1,12 @@
-minetest.register_on_joinplayer(function(player)
-    player:get_inventory():set_size("main", 1)
-    player:hud_set_hotbar_itemcount(1)
-end)
+
+if (not editor_mode) then
+    minetest.register_on_joinplayer(function(player)
+        player:get_inventory():set_size("main", 1)
+        player:hud_set_hotbar_itemcount(1)
+    end)
+else
+    minetest.register_on_joinplayer(function(player)
+        player:get_inventory():set_size("main", 30)
+        player:hud_set_hotbar_itemcount(30)
+    end)
+end
