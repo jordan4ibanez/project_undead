36c6e0f73b80507f4c4c21408f80628066aaf5f8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-09 11:30:36 -0400

    Add in empty foods

--- mods/item/food_registration.lua
+++ mods/item/food_registration.lua
@@ -9,3 +9,17 @@ register_item({
     texture = "water_bottle.png",
     thirst = 20,
 })
+
+
+
+-- begin empty foods to mirror the regular foods
+
+register_item({
+    name = "empty_chips",
+    texture = "empty_chips.png",
+})
+
+register_item({
+    name = "empty_water_bottle",
+    texture = "empty_water_bottle.png",
+})
