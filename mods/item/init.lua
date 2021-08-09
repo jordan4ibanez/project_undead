eef8e99996ac0a4c9b92e29ba2de8357222d092e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-09 09:23:32 -0400

    refactor item registration

--- mods/item/init.lua
+++ mods/item/init.lua
@@ -1,6 +1,6 @@
 local path = minetest.get_modpath("item").."/"
 
 dofile(path.."register_item.lua")
-dofile(path.."registration.lua")
+dofile(path.."food_registration.lua")
 dofile(path.."gun_registration.lua")
 dofile(path.."override_built_in_item_entity.lua")
