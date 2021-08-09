842ce7454bcb24d2aa4a4d49f7a79500b9dd32dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-08-09 10:12:15 -0400

    Create offset hand inventory

--- mods/player/inventory_adjustments.lua
+++ mods/player/inventory_adjustments.lua
@@ -1,3 +1,4 @@
+local create_detached_inventory = minetest.create_detached_inventory
 
 if (not editor_mode) then
     minetest.register_on_joinplayer(function(player)
@@ -8,5 +9,11 @@ else
     minetest.register_on_joinplayer(function(player)
         player:get_inventory():set_size("main", 30)
         player:hud_set_hotbar_itemcount(30)
+
+        local name = player:get_player_name()
+        
+        -- creates the off hand (left hand) inventory
+        create_detached_inventory("secondary", nil, name)
+        player:get_inventory():set_size("secondary", 1)
     end)
 end
